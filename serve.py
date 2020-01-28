bc57f3b6810648494de5d84d607f5de672f8b59c
MaxMa <xingyangma@126.com>
2020-01-28 11:45:55 +0800

    fix content contains '\n'

--- serve.py
+++ serve.py
@@ -1,6 +1,7 @@
 import fasttext
 import jieba
 from constant import STOPWORDS
+import re
 
 
 def get_model_api():
@@ -11,15 +12,15 @@ def get_model_api():
         preds_label = model.predict(query)
         print(preds_label)
         LABEL_2_CATE = {"__label__1": 'technology',
-                             "__label__2": 'car',
-                             "__label__3": 'entertainment',
-                             "__label__4": 'military',
-                             "__label__5": 'sports'}
+                        "__label__2": 'car',
+                        "__label__3": 'entertainment',
+                        "__label__4": 'military',
+                        "__label__5": 'sports'}
         output = LABEL_2_CATE[str(preds_label[0][0])]
         return output
 
     def preprocessed_text(text):
-        segs = jieba.lcut(text)
+        segs = jieba.lcut(re.sub("\n", "", text))
         segs = list(filter(lambda x: len(x) > 1, segs))
         segs = list(filter(lambda x: x not in STOPWORDS, segs))
         return " ".join(segs)
